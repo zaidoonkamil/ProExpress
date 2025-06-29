a4edf8491b09a953391c2695a07303517392c15b
Zaidoon Kamil <igdghfgjhfgvffv68@gmail.com>
2025-06-29 18:20:56 +0300

    .

--- models/ads.js
+++ models/ads.js
@@ -1,7 +1,12 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../config/db");
 
-const Ads = sequelize.define("ads", {
+const Ads = sequelize.define("Ads", {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
     images: {
         type: DataTypes.JSON,
         allowNull: false
