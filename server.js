191a0ef0e88f429a166aca64d44053f2298849b3
Zaidoon Kamil <igdghfgjhfgvffv68@gmail.com>
2025-07-02 21:03:52 +0300

    .

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ const notifications = require("./routes/notifications.js");
 const cors = require("cors");
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
+
 app.use(express.json());
 app.use("/uploads", express.static("./" + "uploads"));
 sequelize.sync({  alter: true })
